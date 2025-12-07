57702f51b05bc5e3bd638195b7b32a48be198762
ADIOCLASSMATE <cqzxwjx@163.com>
2025-12-07 15:27:52 +0000

    path update

--- test_r1.sh
+++ test_r1.sh
@@ -9,5 +9,5 @@ export PORT=8000
 export MODEL_PATH=${BASE_URL}/${MODEL}
 # 从路径名称提取模型名称(DeepSeek-R1)
 export MODEL_NAME=$(basename ${MODEL})
-# bash deploy_vllm.sh
+bash deploy_vllm.sh
 bash internbootcamp/bootcamps/rectangle_count/scripts/eval.sh
